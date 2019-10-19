8313e0fde7c57bd058fb897cc229d1f2c27850e6
HungNhOnPV <anhhungx9propro@gmail.com>
2019-10-19 16:00:20 +0700

    component

---
+++ my-todo-list/src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+    render() {
+        return (
+            <div class = "TodoItem">
+                <p>Đi chợ</p>
+            </div>
+        );
+    }
+}
+
+export default TodoItem;
