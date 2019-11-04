4c1e96309750bcc0d7081cdeb2ae508bdd1d6df3
HungNhOnPV <anhhungx9propro@gmail.com>
2019-11-04 17:01:09 +0700

    lifecycle

---
+++ demo-lifecycle/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import Counter from './components/Counter';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCounter: true
+    }
+  }
+
+  componentDidUpdate() {
+    console.log('App update');
+  }
+
+  removeCounter() {
+    this.setState({
+      showCounter: !this.state.showCounter
+    });
+  }
+
+  render() {
+    console.log('App render');
+    return (
+      <div className="App">
+        <button onClick={() => this.removeCounter()}>Remove counter</button>
+        { this.state.showCounter && < Counter /> }
+      </div>
+    );
+  }
+}
+
+export default App;
