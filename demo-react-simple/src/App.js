8e3783a21b288b28eac896bf2b394cddfff528f8
HungNhOnPV <anhhungx9propro@gmail.com>
2020-01-19 15:16:03 +0700

    demo state

--- demo-react-simple/src/App.js
+++ demo-react-simple/src/App.js
@@ -6,55 +6,68 @@ import FromAddProduct from './components/FormAddProduct';
 
 import Iphone from './images/Iphone-XS-Max.png';
 
-function App() {
-    const products = [
-        {
-            id: 1,
-            name: 'Iphone XS Max',
-            image: Iphone,
-            price: 2000,
-            status: true
-        },
-        {
-            id: 2,
-            name: 'Iphone XS',
-            image: Iphone,
-            price: 3000,
-            status: true
-        },
-        {
-            id: 3,
-            name: 'Samsung galaxy S10',
-            image: Iphone,
-            price: 2500,
-            status: false
-        },
-        {
-            id: 4,
-            name: 'Nokia Lumia',
-            image: Iphone,
-            price: 2100,
-            status: true
-        },
-        {
-            id: 5,
-            name: 'Oppo F1s',
-            image: Iphone,
-            price: 1500,
-            status: true
-        },
-        {
-            id: 6,
-            name: 'B phone 3',
-            image: Iphone,
-            price: 1000,
-            status: true
-        }
-    ];
+class App extends React.Component {
 
-    const elements = products.map((product, index) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: [
+                {
+                    id: 1,
+                    name: 'Iphone XS Max',
+                    image: Iphone,
+                    price: 2000,
+                    status: true
+                },
+                {
+                    id: 2,
+                    name: 'Iphone XS',
+                    image: Iphone,
+                    price: 3000,
+                    status: true
+                },
+                {
+                    id: 3,
+                    name: 'Samsung galaxy S10',
+                    image: Iphone,
+                    price: 2500,
+                    status: false
+                },
+                {
+                    id: 4,
+                    name: 'Nokia Lumia',
+                    image: Iphone,
+                    price: 2100,
+                    status: true
+                },
+                {
+                    id: 5,
+                    name: 'Oppo F1s',
+                    image: Iphone,
+                    price: 1500,
+                    status: true
+                },
+                {
+                    id: 6,
+                    name: 'B phone 3',
+                    image: Iphone,
+                    price: 1000,
+                    status: true
+                }
+            ],
+            isActive: true
+        };
+    }
+
+    onSetState = () => {
+        this.setState({
+            isActive: !this.state.isActive
+        });
+    }
+
+    elements = () => this.state.products.map((product, index) => {
         let result = '';
-        if(product.status) {
+        if(this.state.isActive) {
             result =    <Product
                             key={ product.id }
                             price={ product.price }
@@ -66,19 +79,26 @@ function App() {
         return result;
     });
 
+    render() {
         return(
             <div className="App">
                 < Header />
                 <div className="row">
-                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <FromAddProduct />
                     </div>
                     <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        { elements }
+                        { this.elements() }
                     </div>
                 </div>
-            </div>           
+                <div>
+                    <button type="button" className="btn btn-warning center-block" onClick={ this.onSetState } >
+                        Active: { this.state.isActive ? 'true' : 'false' }
+                    </button>
+                </div>
+            </div>                    
             );
+    }
 }
 
 export default App;
