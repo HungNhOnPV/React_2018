081dc130c9448ec65f818da3c94aff0c5961f758
HungNhOnPV <anhhungx9propro@gmail.com>
2020-02-03 20:40:12 +0700

    products container

--- project-cart/src/components/Products.js
+++ project-cart/src/components/Products.js
@@ -1,30 +1,15 @@
 import React from "react";
-import Product from "./Product";
 
 class Products extends React.Component {
 
-  showProducts = products => {
-    let result = null;
-    if(products.length > 0) {
-      result = products.map((product, index) => {
-        return <Product 
-                  key ={index}
-                  product={product}
-                ></Product>
-      });
-    }
-    return result;
-  }
-
   render() {
-    const { products } = this.props;
     return (
       /* <!-- Products --> */
       <section className="section">
         <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
         <div className="row">
           {/* <!-- Product --> */}
-          { this.showProducts(products) }
+          { this.props.children }
         </div>
       </section>
     );
--- project-cart/src/containers/ProductsContainer.js
+++ project-cart/src/containers/ProductsContainer.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import Products from '../components/Products';
+import Product from '../components/Product';
 
 class ProductsContainer extends Component {
+
+    showProducts = products => {
+        let result = null;
+        if(products.length > 0) {
+          result = products.map((product, index) => {
+            return <Product 
+                      key ={index}
+                      product={product}
+                    ></Product>
+          });
+        }
+        return result;
+      }
+
     render() {
         const { products } = this.props;
         return (
-            <Products products={products} />
+            <Products>
+                { this.showProducts(products) }
+            </Products>
         );
     }
 }
