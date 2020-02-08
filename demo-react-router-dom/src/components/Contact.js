fb2b3c0e8ad98693bc3103c6f256c32b38e513fc
HungNhOnPV <anhhungx9propro@gmail.com>
2020-02-08 16:50:28 +0700

    object prompt in router

--- demo-react-router-dom/src/components/Contact.js
+++ demo-react-router-dom/src/components/Contact.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import { Prompt } from 'react-router-dom';
 
 class Contact extends Component {
+    state = {
+        isChecked: false
+    }
+
+    onClick = isChecked => {
+        this.setState({
+            isChecked: isChecked
+        });
+    }
+
     render() {
+        const { isChecked } = this.state;
         return(
-            <div>Day la trang lien he</div>
+            <div>
+                Day la trang lien he<br />
+                <button 
+                    type="button" 
+                    className="btn btn-info" 
+                    onClick={ () => this.onClick(false) } 
+                >
+                    Cho phep
+                </button>&nbsp;
+                <button 
+                    type="button" 
+                    className="btn btn-danger" 
+                    onClick={ () => this.onClick(true) } 
+                >
+                    Khong cho phep
+                </button>
+                <Prompt
+                    when={isChecked}
+                    message={ location => (`Ban chac chan muon di den ${location.pathname}`)}
+                />
+            </div>
+            
         );
     }
 }
